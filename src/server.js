6c9b0461f042ac9324982cb32331b107b6738516
Carlos Andres Toro Guerrero <carlostoro04@gmail.com>
2024-10-26 18:31:25 -0500

    Remover products esnpoint

--- src/server.js
+++ src/server.js
@@ -12,14 +12,6 @@ app.get('/api/user', (req, res) => {
     });
 });
 
-app.get('/api/products', (req, res) => {
-    res.json([
-        { id: 1, name: 'Product 1', price: 100 },
-        { id: 2, name: 'Product 2', price: 200 },
-        { id: 3, name: 'Product 3', price: 300 },
-    ]);
-});
-
 app.get('/api/stores', (req, res) => {
     res.json([
         { id: 1, name: 'Store 1', location: 'New York' },
